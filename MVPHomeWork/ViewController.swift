f872e449e54a350c2c2844ed5be6572c5316fc3c
Ilya <izheltikov0@gmail.com>
2022-06-04 13:55:13 +0300

    Initial Commit

---
+++ MVPHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPHomeWork
+//
+//  Created by Илья Желтиков on 04.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
